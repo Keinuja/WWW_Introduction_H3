45660bc481ed5cf5f0693a14d014ca517977e5a6
Keinuja <46192497+keinuja@users.noreply.github.com>
2022-09-23 13:41:51 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+const osoite =
+  "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff";
+
+async function getapi(osoite) {
+  const vastaus = await fetch(osoite);
+  var data = await vastaus.json();
+  nayta(data);
+}
+
+function nayta(data) {
+  let taulukko = `<thead id="OtsikkoKohde">
+    <th>Municipality</th>
+    <th>Population</th>
+  </thead>`;
+
+  for (let r of data.list) {
+    taulukko += `<tr>
+        <td>${r.dataset.dimension.Alue.category.label}</td>
+        <td>${r.dataset.value}</td>
+      </tr>`;
+  }
+  document.getElementById("HTML-taulukko").innerHTML = taulukko;
+}
+
+getapi(osoite);
